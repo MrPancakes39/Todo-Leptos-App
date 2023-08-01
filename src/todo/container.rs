7546580f4ecd6a3a8be86e835ee02adaf2c0151c
MrPancakes39 <salmannhassan39@gmail.com>
2023-08-01 23:32:44 +0300

    Added Basic HTML Structure

--- src/todo/container.rs
+++ src/todo/container.rs
@@ -1,3 +1,30 @@
 use leptos::*;
 
-pub fn TodoContainer(cx: Scope) -> impl IntoView {}
+#[derive(Debug, Clone)]
+struct TodoItem {
+    text: String,
+    key: u32,
+}
+
+pub fn TodoContainer(cx: Scope) -> impl IntoView {
+    let (todos, set_todos) = create_signal(cx, Vec::<TodoItem>::new());
+
+    view! {cx,
+        <form class="todo-container">
+            <h4 class="title">"Cool Todo App"</h4>
+            <div class="form-group">
+                <input placeholder="Add a todo item" id="todo_input" />
+                <button type="button">"+"</button>
+            </div>
+            <div class="todos">
+                <For each=move||todos.get() key=|todo|todo.key view=move|cx, todo|{} />
+            </div>
+            <div class="form-group">
+                <p>"You have "{move||todos.get().len()}" pending tasks"</p>
+                <button type="button" id="clear" on:click=move|_|set_todos.update(|v| v.clear())>
+                    "Clear All"
+                </button>
+            </div>
+        </form>
+    }
+}
